947c335f77823a3321d277cd2464e4dd9a55ff56
peersmg <peersmg@gmail.com>
2019-10-01 12:31:01 +0100

    [1.4] Menu class created

---
+++ src/com/twu/biblioteca/Menu.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+import java.util.List;
+
+public class Menu {
+    List<Book> books;
+
+    public Menu(List<Book> books) {
+        this.books = books;
+    }
+
+    
+}
