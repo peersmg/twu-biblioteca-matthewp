96aa5e16a6cdc1225a8073502c331ae4810f06ff
peersmg <peersmg@gmail.com>
2019-10-05 16:10:55 +0100

    [Feedback] Extracted selection to var in menu tests

--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -15,6 +15,8 @@ import static org.junit.Assert.assertThat;
 
 public class MenuTest {
     private Menu testMenu;
+    private static final int PRINT_ALL_SELECTION = 1;
+    private static final int PRINT_CHECKED_OUT_SELECTION = 3;
 
     @Before
     public void init(){
@@ -31,7 +33,7 @@ public class MenuTest {
         dummyBooks.addBook(new Book(2,"Test1", "Matt1", 2019, false));
         testMenu.setBooks(dummyBooks);
 
-        assertThat(testMenu.actOnSelection(1), is("1 | Test | Matt | 2019\n2 | Test1 | Matt1 | 2019\n"));
+        assertThat(testMenu.actOnSelection(PRINT_ALL_SELECTION), is("1 | Test | Matt | 2019\n2 | Test1 | Matt1 | 2019\n"));
     }
 
     @Test
@@ -41,7 +43,8 @@ public class MenuTest {
         dummyBooks.addBook(new Book(2,"Test1", "Matt1", 2019, true));
         testMenu.setBooks(dummyBooks);
 
-        assertThat(testMenu.actOnSelection(1), is("1 | Test | Matt | 2019\n"));
+        int printBookSelection = 1;
+        assertThat(testMenu.actOnSelection(PRINT_ALL_SELECTION), is("1 | Test | Matt | 2019\n"));
     }
 
     @Test
@@ -51,6 +54,6 @@ public class MenuTest {
         dummyBooks.addBook(new Book(2,"Test1", "Matt1", 2019, true));
         testMenu.setBooks(dummyBooks);
 
-        assertThat(testMenu.actOnSelection(3), is("2 | Test1 | Matt1 | 2019\n"));
+        assertThat(testMenu.actOnSelection(PRINT_CHECKED_OUT_SELECTION), is("2 | Test1 | Matt1 | 2019\n"));
     }
 }
