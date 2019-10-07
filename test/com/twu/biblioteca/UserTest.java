691e2d839c77a2aed2eb72c7399c4301e9adfd79
Matthew Peers <matthewpeers@Matthews-MBP.lan>
2019-10-07 21:56:13 +0100

    [2.3] User unit tests created in prep for User class

---
+++ test/com/twu/biblioteca/UserTest.java
@@ -0,0 +1,21 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class UserTest {
+
+    @Test
+    public void canValidateUser()
+    {
+        User testUser = new User("111-1111", "password");
+
+        boolean loginSuccess = testUser.validate("password");
+        boolean loginFails = testUser.validate("wrong-password");
+
+        assertThat(loginSuccess, is(true));
+        assertThat(loginFails, is(true));
+    }
+}
