070e950f94e30bc4a20c14d4460852ac5fe27f14
Matthew Peers <peersmg@gmail.com>
2019-10-07 22:25:03 +0100

    [2.3] UserRegister unit tests created in prep for UserRegister class

---
+++ test/com/twu/biblioteca/UserRegisterTest.java
@@ -0,0 +1,35 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class UserRegisterTest {
+    @Test
+    public void canRegisterUser()
+    {
+        UserRegister userRegister = new UserRegister();
+
+        User newUser = new User("111-1111", "password");
+        userRegister.addUser(newUser);
+
+        boolean userExists = userRegister.userExists("111-1111");
+
+        assertThat(userExists, is(true));
+    }
+
+    @Test
+    public  void canGetUser()
+    {
+        UserRegister userRegister = new UserRegister();
+
+        User newUser = new User("111-1111", "password");
+        userRegister.addUser(newUser);
+
+        User loggedUser = userRegister.getUser("111-1111", "password");
+
+        assertThat(newUser, is(loggedUser));
+    }
+
+}
