a4ddfae825a3cffcbda3b2b83e56381b620c0624
Matthew Peers <peersmg@gmail.com>
2019-10-08 14:52:45 +0100

    [2.4] New User unit tests in prep for additional details

--- test/com/twu/biblioteca/UserTest.java
+++ test/com/twu/biblioteca/UserTest.java
@@ -1,21 +1,44 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class UserTest {
-
+    User testUser;
+    @Before
+    public void init()
+    {
+        testUser = new User("111-1111", "password", "Matt", "email@email.com", "07965330222");
+    }
     @Test
     public void canValidateUser()
     {
-        User testUser = new User("111-1111", "password");
-
         boolean loginSuccess = testUser.validate("password");
         boolean loginFails = testUser.validate("wrong-password");
 
         assertThat(loginSuccess, is(true));
         assertThat(loginFails, is(false));
+    }
+
+    @Test
+    public void canGetUserName()
+    {
+        assertThat("Matt", containsString(testUser.getName()));
+    }
+
+    @Test
+    public void canGetUserEmail()
+    {
+        assertThat("email@email.com", containsString(testUser.getEmail()));
+    }
+
+    @Test
+    public void canGetUserPhoneNumber()
+    {
+        assertThat("07965330222", containsString(testUser.getPhoneNumber));
     }
 }
